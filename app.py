a5e6f08b0beca8cefcd852eab64845f177631fe8
ajcloete <andre.cloete@anmut.co.uk>
2020-12-10 02:30:14 +0200

    fix models and generate migrations

--- app.py
+++ app.py
@@ -36,36 +36,45 @@ manager.add_command("db", MigrateCommand)
 
 
 class Venue(db.Model):
-    __tablename__ = "Venue"
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String))
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.BOOLEAN)
+    seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship("Show", backref="venue")
 
 
 class Artist(db.Model):
-    __tablename__ = "Artist"
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.BOOLEAN)
+    seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship("Show", backref="artist")
 
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"), nullable=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=True)
+    start_time = db.Column(db.TIMESTAMP)
+
 
 # ----------------------------------------------------------------------------#
 # Filters.
